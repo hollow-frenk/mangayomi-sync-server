cb8e59b508a899780009cc9d7f06c9d39f7dc945
Francesco De Feo <francesco.defeo95@icloud.com>
2024-11-19 10:09:37 +0100

    update

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-/app/tailscale up --auth-key=${TAILSCALE_AUTH_KEY} --hostname=${TAILSCALE_HOST_NAME}
